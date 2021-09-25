7378dac44034b3226f7ebcd982ca300512bb0d6a
mahmoud-irshaid <mirshaid10@gmail.com>
2021-09-25 21:54:12 +0300

    Updates

---
+++ static/js/8.31e24ea7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwpm-app"]=this["webpackJsonpwpm-app"]||[]).push([[8],{34:function(p,t,n){"use strict";n.r(t);n(0);var s=n(1);t.default=function(p){var t=p.time;return Object(s.jsx)("span",{children:t})}}}]);
+//# sourceMappingURL=8.31e24ea7.chunk.js.map
